88f1bfe5f293a8b447b74e2b04b15c209cef0b85
Vanilla <neko@hydev.org>
2021-08-07 21:23:43 +0800

    [+] Add package nano & nanorc, also enable it.

--- cli.nix
+++ cli.nix
@@ -6,5 +6,11 @@
 
     pkgs.nodejs
     pkgs.nodePackages.pxder
+
+    pkgs.nano
+    pkgs.nanorc
   ];
+
+  # https://gist.github.com/JeffreyCA/321f9e704e5561d60f90d9f3a923a0ac
+  home.file.".nanorc".text = "include ${pkgs.nanorc}/share/*.nanorc";
 }
