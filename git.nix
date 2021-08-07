530d334e09f68bef314593602fea6f262acdb8f5
Vanilla <neko@hydev.org>
2021-08-07 14:44:17 +0800

    [+] Add config for git proxy, replace old netcat of macOS.

--- git.nix
+++ git.nix
@@ -1,6 +1,14 @@
-{ programs, ... }:
+{ programs, pkgs, ... }:
 {
   programs.git.enable = true;
   programs.git.userName = "Vanilla";
   programs.git.userEmail = "neko@hydev.org";
+
+  home.packages = [ pkgs.netcat ];
+  programs.ssh.enable = true;
+  programs.ssh.matchBlocks."git@github.com" = {
+    host = "github.com";
+    user = "git";
+    proxyCommand = "nc -X connect -x localhost:7890 %h %p";
+  };
 }
