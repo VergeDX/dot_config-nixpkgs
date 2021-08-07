7f1be87a8ba7cdd89959d66ea8a76a2ef9756ed5
Vanilla <neko@hydev.org>
2021-08-07 14:09:57 +0800

    [+] Adding fish plugin: replay, then init nix inx fish.

--- fish.nix
+++ fish.nix
@@ -1,4 +1,4 @@
-{ programs, ... }:
+{ programs, pkgs, ... }:
 {
   programs.fish.enable = true;
   programs.starship.enable = true;
@@ -6,4 +6,17 @@
     add_newline = false;
     line_break = { disabled = true; };
   };
+
+  # https://nixos.org/download.html
+  programs.fish.shellInit = "replay . /Users/vanilla/.nix-profile/etc/profile.d/nix.sh";
+  programs.fish.plugins = [
+    {
+      name = "replay.fish";
+      src = pkgs.fetchgit {
+        url = "https://github.com/jorgebucaran/replay.fish";
+        rev = "1.2.0";
+        sha256 = "sha256-Q/9YVdiRSJw1SdcfQv2h7Lj6EyFustRk+kmh1eRRQ6k=";
+      };
+    }
+  ];
 }
