3492cde2bfb380d47afb260da4ad714e2e2ea893
Vanilla <neko@hydev.org>
2021-08-07 15:08:39 +0800

    [+] Add config for terminal proxy, using to hm switch command.

--- home.nix
+++ home.nix
@@ -25,4 +25,10 @@
     ./fish.nix
     ./cli.nix
   ];
+
+  home.sessionVariables = {
+    https_proxy = "http://127.0.0.1:7890";
+    http_proxy = "http://127.0.0.1:7890";
+    all_proxy = "socks5://127.0.0.1:7890";
+  };
 }
