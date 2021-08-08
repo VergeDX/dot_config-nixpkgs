fb847a6170abac178ce3c3c8a8ec1965d57fda26
Vanilla <neko@hydev.org>
2021-08-09 06:34:07 +0800

    [+] Add fish complition for Homebrew, but no effect.

--- fish.nix
+++ fish.nix
@@ -11,5 +11,9 @@ in
   programs.fish.shellInit = ''
     replay source /etc/static/bashrc
     thefuck --alias | source
+
+    # https://docs.brew.sh/Shell-Completion
+    # set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+    # set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
   '';
 }
