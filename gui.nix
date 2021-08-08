2eff00831393976521c9f5c0a7c82bdf33d3796e
Vanilla <neko@hydev.org>
2021-08-08 13:47:35 +0800

    [-] Remove config of kitty, it cannot switch light & dark mode.

--- gui.nix
+++ gui.nix
@@ -1,12 +1,4 @@
 { home, pkgs, ... }:
 {
   home.packages = [ pkgs.gimp ];
-
-  programs.kitty.enable = true;
-  programs.kitty.font = { package = pkgs.hack-font; name = "Hack"; };
-  programs.kitty.extraConfig = "include ./theme.conf";
-  home.file.".config/kitty/theme.conf".source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/Solarized_Light.conf";
-    sha256 = "sha256-1UN/jP63IqA0L3848pAjoeeG/uyoVD91zBexXzD3tvU=";
-  };
 }
