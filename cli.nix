6c4a8c5c4713132beb17cbdba89e47a80c23c2cd
Vanilla <neko@hydev.org>
2021-08-08 08:49:20 +0800

    [+] Enable autojump for fish shell.

--- cli.nix
+++ cli.nix
@@ -13,4 +13,7 @@
 
   # https://gist.github.com/JeffreyCA/321f9e704e5561d60f90d9f3a923a0ac
   home.file.".nanorc".text = "include ${pkgs.nanorc}/share/*.nanorc";
+
+  programs.autojump.enable = true;
+  programs.autojump.enableFishIntegration = true;
 }
