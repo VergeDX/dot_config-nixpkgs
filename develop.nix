862cbf2bfa6cdf1744b066a12d58297b6a5f744a
Vanilla <neko@hydev.org>
2021-08-09 23:47:08 +0800

    [+] Config of session variable: enable config for electron.

--- develop.nix
+++ develop.nix
@@ -10,4 +10,10 @@
   home.file.".config/flutter/settings".text = builtins.toJSON {
     "enable-macos-desktop" = true;
   };
+
+  # https://stackoverflow.com/questions/60054531/how-can-i-solve-the-connection-problem-during-npm-install-behind-a-proxy
+  home.sessionVariables = {
+    ELECTRON_GET_USE_PROXY = true;
+    GLOBAL_AGENT_HTTPS_PROXY = "http://127.0.0.1:7890";
+  };
 }
