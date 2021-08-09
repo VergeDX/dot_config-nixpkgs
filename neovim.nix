b5ee7c77035c7b3ba9cae0867b5b7394e7ed2cd4
Vanilla <neko@hydev.org>
2021-08-09 08:27:00 +0800

    [M] Neovim config: Changes a method to get light & dark status (command return value).

--- neovim.nix
+++ neovim.nix
@@ -2,12 +2,14 @@
 {
   # https://arslan.io/2021/02/15/automatic-dark-mode-for-terminal-applications/
   # https://neovim.io/doc/user/autocmd.html
+  # https://vi.stackexchange.com/questions/19773/get-exit-status-from-system-call
   programs.neovim.extraConfig = ''
     function! MatchColorScheme()
-      if system("defaults read -g AppleInterfaceStyle") =~ '^Dark'
-        colorscheme emilia_dark
-      else
+      let output = system("defaults read -g AppleInterfaceStyle")
+      if v:shell_error != 0
         colorscheme emilia_light
+      else
+        colorscheme emilia_dark
       endif
     endfunction
 
