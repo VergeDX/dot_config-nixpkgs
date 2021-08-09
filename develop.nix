286800f3dc36c6f59a83648453b93fad08a5e66c
Vanilla <neko@hydev.org>
2021-08-09 16:37:12 +0800

    [+] Enable macos desktop for flutter, add brew openjdk@8 comment.

--- develop.nix
+++ develop.nix
@@ -5,5 +5,9 @@
     pkgs.yarn
   ];
 
+  # https://formulae.brew.sh/formula/openjdk@8
   home.sessionPath = [ "/Users/vanilla/Android/platform-tools" ];
+  home.file.".config/flutter/settings".text = builtins.toJSON {
+    "enable-macos-desktop" = true;
+  };
 }
