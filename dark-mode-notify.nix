5b18d6d1320fb564e999b0cd44fae692ed59b2bc
Vanilla <neko@hydev.org>
2021-08-16 21:42:04 +0800

    [-] dark-mode-notify.nix: remove logging.

--- dark-mode-notify.nix
+++ dark-mode-notify.nix
@@ -1,6 +1,4 @@
 { home, pkgs, ... }:
-let dmn_repo_folder = "/Users/vanilla/Documents/dark-mode-notify";
-in
 {
   # https://github.com/bouk/dark-mode-notify
   # https://arslan.io/2021/02/15/automatic-dark-mode-for-terminal-applications/
@@ -13,10 +11,6 @@ in
         <string>ke.bou.dark-mode-notify</string>
         <key>KeepAlive</key>
         <true/>
-        <key>StandardErrorPath</key>
-        <string>${dmn_repo_folder}/dark-mode-notify-stderr.log</string>
-        <key>StandardOutPath</key>
-        <string>${dmn_repo_folder}/dark-mode-notify-stdout.log</string>
         <key>ProgramArguments</key>
         <array>
           <string>/usr/local/bin/dark-mode-notify</string>
