ea786368df346e3f324fc00d780f6fb092069b98
Vanilla <neko@hydev.org>
2021-08-17 11:20:18 +0800

    [+] Package steam theme: metro-for-steam.nix, and apply it.

--- gui.nix
+++ gui.nix
@@ -1,4 +1,7 @@
 { home, pkgs, ... }:
+let metro-for-steam = pkgs.callPackage ./metro-for-steam.nix { };
+in
 {
   home.packages = [ pkgs.gimp ];
+  home.file."Library/Application Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/skins/".source = metro-for-steam;
 }
