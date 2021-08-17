073983163015c4a890aad5ef761d9263313b184e
Vanilla <neko@hydev.org>
2021-08-17 17:55:23 +0800

    [+] flake.nix: import deploy-rs.

--- flake.nix
+++ flake.nix
@@ -1,12 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
+
+    # https://github.com/NickCao/flakes/blob/master/flake.nix
+    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, rust-overlay, ... }:
+  outputs = { self, nixpkgs, home-manager, rust-overlay, deploy-rs, ... }:
     let system = "x86_64-darwin";
     in
     {
@@ -20,11 +26,11 @@
           configuration = { pkgs, ... }: {
             imports = [ ./home.nix ];
 
-            nixpkgs.overlays = [ rust-overlay.overlay ];
+            nixpkgs.overlays = [ rust-overlay.overlay deploy-rs.overlay ];
             home.packages = [
               pkgs.rust-bin.stable.latest.default
               pkgs.rust-bin.stable.latest.rust-src
-            ];
+            ] ++ [ pkgs.deploy-rs.deploy-rs ];
           };
         };
     };
