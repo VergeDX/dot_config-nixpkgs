b0143f669a5ea1524e85b2baa0a378af1961ead6
Vanilla <neko@hydev.org>
2021-08-23 08:38:32 +0800

    [+] Sync upstream neovim config, tons of plugins.

--- home.nix
+++ home.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:
-
 let
   vergedx_config_baseurl = "https://github.com/VergeDX/config-nixpkgs/raw/master/config";
   neovim_config = (builtins.fetchurl {
     url = "${vergedx_config_baseurl}/neovim.nix";
-    sha256 = "08mxq3p2i5iy7wq0incby5bhgd5xy22332sxsgdpcmp6iq9rq6lh";
+    sha256 = "022pyndxnq6ydfdrd0mfmblxywkvqb77m1p538v6y55500w8959n";
   });
   shells_config = (builtins.fetchurl {
     url = "${vergedx_config_baseurl}/shells.nix";
